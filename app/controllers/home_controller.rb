8c2a2079e3d1328552db50a331ae1b1018780538
Barunabha Poddar <barun1996poddar@gmail.com>
2020-04-24 22:09:24 +0530

    Csv downloader revised

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def users
+    @users = User.order(:user_name)
+    respond_to do |format|
+      format.html
+      format.csv { send_data @users.to_csv, filename: "users.csv" }
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ApplicationRecord
-    def self.import(file)
-        CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
-            User.create! row.to_hash
-        end
-    end
-
-    def self.to_csv(fields = column_names, options={})
-      CSV.generate(options) do |csv|
-        csv << column_names
-        all.each do |user|
-          csv << user.attributes.values_at(*column_names)
-        end
+  def self.to_csv
+    attributes = %w{user_name user_email phone_number}
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.each do |user|
+        csv << attributes.map{ |attr| user.send(attr) }
       end
     end
+  end
 end
