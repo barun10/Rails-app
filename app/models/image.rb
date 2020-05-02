ca64572d8ce68984e0de593ad45ef5a4a829cd00
Barunabha Poddar <barun1996poddar@gmail.com>
2020-05-02 09:03:14 +0530

    small change

--- app/models/image.rb
+++ app/models/image.rb
@@ -10,7 +10,7 @@ class Image < ApplicationRecord
         if image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
             errors.add(:image, 'must be jpg or png')
         elsif image.attached? == false
-            errors.add(:image, 'required')
+            errors.add(:image, 'image required')
         end
     end
 
