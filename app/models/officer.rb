7b549c48f209d03c918918e500583e11086ff475
Barunabha Poddar <barun1996poddar@gmail.com>
2020-05-05 20:45:25 +0530

    fixing case sensitivity

--- app/models/officer.rb
+++ app/models/officer.rb
@@ -1,11 +1,11 @@
 class Officer < ApplicationRecord
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 		validates :name,	presence: true,
-											uniqueness: true
+											uniqueness: {case_sensitive: false}
 
 
 		validates :email, presence: true,
-											uniqueness: true,
+											uniqueness: {case_sensitive: false},
 											format: { with: EMAIL_REGEX }
 
 end
